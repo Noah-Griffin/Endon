0d58b5e26f2df7f9ba0ca024cb190ca91ecca26c
Noah-Griffin <47569824+Noah-Griffin@users.noreply.github.com>
2021-05-03 16:43:07 +0100

    Endon version 1

---
+++ ENDON/arrays.js
@@ -0,0 +1,84 @@
+var boxes = [];
+
+
+var narrative = [
+
+['Upon entering the room, separate groups of people had already flocked together into different sections of the space with the others floating in between.',
+'Everyone had already decided socially where they were situated.'],
+
+['The groups were distinguishable by their style of dress.',
+'Endon felt alien.', 'bottom', 'https://www.youtube.com/watch?v=eRvfxWRi6qQ'],
+
+['70’s retro teenagers.',
+'Reviving their parents wardrobes.'],
+
+['People who all wore t-shirts with black squares where there should be a logo.',
+'Apparently a statement.'],
+
+['Middle aged, melting punks.',
+'They had failed to break the system.'],
+
+['Uniformed personnel.',
+'They liked being told to tell people what to do.'],
+
+['People who wore something that couldn’t be described yet.',
+'Yet it actually felt very familiar.'],
+
+['Rich people.',
+'Who didn’t want you to know it.'],
+
+['Athletes.',
+'Warming up.'],
+
+['Everyone drank orange juice mixed with whatever styles of drug they decided to bring. This resulted in a wide array of behaviour to be exhibited during the course of the night.',
+'Everyone knew their limits.'],
+
+['The groups separated and reformed based on the human-chemical hybrids that had emerged.',
+'Sweaty circles of people professing their love contrasted the disassociated.'],
+
+['In an events based culture, the idea of not being intoxicated is ridiculous.',
+'And quite frankly scared Endon. Everything was an event and should be experienced in the widest array of states possible.'],
+
+['The athletes stayed sober.',
+'They were already at their peak physiological states.'],
+
+['Then the music started. The dance floor was filled in a gradient from uppers on the right to downers on the left. The music matched this arrangement. Ambient effects played at one end, bass and drums at the other.',
+'A spectrum of intensity.'],
+
+['The energy of the room trickled down from right to left.',
+'It was loaned and passed down from those on your right to those on your left. Everyone sat within their space.'],
+
+['The adventurous ones who dared to move further right quickly returned to their original positions.',
+'Equilibrium.'],
+
+['The middle was the sweet spot for the perfectionist.',
+'If you span around as quickly as you could, the song sounded even. But this was besides the point.'],
+
+['It wasn’t sure what era the music came from. Could’ve been 90s, 00’s, 10’s or 20’s.',
+'Locating it wouldn’t have given it anymore significance. And people were too far gone to care.'],
+
+['It was tinged with this sickly feeling that it wanted you to like it. Someone should have asked it: how can I love you when you can’t love yourself?',
+'It’s not me its you.'],
+
+['At one point the music stopped and 3 band members got up on stage. One played the theremin, one played the trombone, the other played the pheromone.',
+'The 3rd was definitely the sex appeal.'],
+
+['They each wore t-shirts that displayed vaguely provocative 4 letter words.',
+'The words need not be spelled out.'],
+
+['The walls moved around to form small alcoves where people could sit and have private conversations about important things like work, movies or the environment.',
+'These conversations were mainly for show and didn’t last long.'],
+
+['Endon hated being asked what he did.',
+'He was hesitant to even call it work. Certainly not a career.'],
+
+['Endon was a writer but distinctly not an author.',
+'He had no authority contrary to popular belief.'],
+
+['He preferred the title writer because of the mechanical implications it carried. Information was presented to him and he translated it into his writing.',
+'From the outside, it was known that information was lost in translation but he couldn’t help it.'],
+
+['People often felt paranoid about what Endon wrote about them but due to his foreign language, they couldn’t actually know what was said or not said.',
+'This didn’t stop them trying to figure it out.']
+
+];
