df844c583539d8d35953287d60344aa01be73c98
Noah-Griffin <47569824+Noah-Griffin@users.noreply.github.com>
2021-05-03 17:10:49 +0100

    Update arrays.js

--- arrays.js
+++ arrays.js
@@ -6,6 +6,6 @@ var narrative = [
   ['Endon.', 'Coming Soon.', 'top', 'noahgriffinart.com'],
   ['Endon.', 'Coming Soon.', 'top', 'noahgriffinart.com'],
   ['Endon.', 'Coming Soon.', 'top', 'noahgriffinart.com'],
-  ['Endon.', 'Coming Soon.', 'top', 'noahgriffinart.com'],
+  ['Endon.', 'Coming Soon.', 'top', 'noahgriffinart.com']
   
 ];
