723d9c33f46e692071c2a96118a46d25b22c300b
Noah-Griffin <47569824+Noah-Griffin@users.noreply.github.com>
2021-10-02 18:09:24 +0100

    Update arrays.js

--- arrays.js
+++ arrays.js
@@ -3,244 +3,98 @@ var boxes = [];
 
 var narrative = [
 
-['Upon entering the room, separate groups of people had already flocked together into different sections of the space with the others floating in between.',
-'Social situation was seemingly prearranged.'],
+['Some context.',
+'Upon entering the room, separate groups of people had already flocked together into different sections of the space with the others floating in between.'],
 
-['The groups were distinguishable by their style_of_dress.',
-'Endon did not gel.'],
+['Maybe it’s useful to start from before, considering some of the most basic means of public communication.',
+'Everyone had already decided socially where they were situated.'],
 
-['Plain clothes.',
-'Apparently a statement.'],
+['Before most of the general public was literate, pictorial/phonetic signage was used as a means of identification.',
+'The groups were distinguishable by their style of dress. '],
 
-['90’s retro teenagers.',
-'Co-opting their parents wardrobes.'],
+['Houses, shops and other physical locations were named with shapes and colours.',
+'Endon felt alien. '],
 
-['Middle aged/melting punks.',
-'Sold out/off.'],
+['However, due to the smallness of cities and the lack of accessible public transport, this form of communication doesn’t actually appear to be that inefficient as colloquialisms form through the communities around the signs.',
+'70’s retro teenagers, reviving their parents wardrobes.'],
 
-['Uniformed personnel.',
-'They liked being told to tell people what to do.', 
+['As spaces grew and mobility increased, these colloquialisms threaten to confuse the outsider.',
+'Middle aged, melting punks.'],
+
+['Numerical systems of naming sites are now introduced, allowing for easier recording, mapping, taxing and monitoring by governing bodies.',
+'Uniformed personnel (they liked being told to tell people what to do)',
 'top', 'https://www.amazon.co.uk/Amosfun-Lingerie-Policeman-Underwear-Boyfriends/dp/B08253F4N3/ref=sr_1_16?crid=28WXRGV1A3DO7&dchild=1&keywords=sexy+police+outfit&qid=1620296419&sprefix=sexy+police%2Caps%2C156&sr=8-16'],
 
-['People who wore something that couldn’t be described yet.',
-'Unless following certain threads.'],
+['We see here the adoption of a dominant language by the masses that offers both efficiency and increased manageability. ',
+'People who wore something that couldn’t be described yet.'],
 
-['Rich people.',
-'Who didn’t want u to know it.'],
+['This efficiency has been picked up and hailed by the romantic-utopian modernists through manifestos, proposals and practice, a form-follows-function utilitarianism that has carried through into current digital spaces.',
+'Rich people who didn’t want you to know it.'],
 
-['Athletes.',
-'Streched to the limit.'],
+['Fast forward to a postmodern rejection of this structurally enforced efficiency.',
+'Athletes warming up.'],
 
-['Everyone drank orange juice mixed with a tailored combination of substances.',
-'Everyone knew their limits.'],
+['We are faced with an issue of measurement.',
+'Everyone drank orange juice mixed with whatever substances they decided to bring. This resulted in a wide array of behaviour to be exhibited during the course of the night.'],
 
-['The groups seperated/reformed/human/chemical hybrids.',
-'Sweaty circles of people professing their love contrasted the disassociated.'],
-
-['In an events based culture such as this, the idea of not being intoxicated is stupid.',
-'And quite frankly scared Endon. Everything was an event and should be experienced in the widest array of states possible.',
+['One example to consider may be the 24 hour clock.',
+'Everyone knew their limits.',
 'top', 'https://www.youtube.com/watch?v=azwXuTIEyto'],
 
-['The athletes stayed sober.',
-'They were already at their peak physiological states.'],
+['Fast forward to the rave.',
+'In an events based culture, the idea of not being intoxicated is ridiculous.'],
 
-['Then the music started. The dance floor was filled in a gradient from uppers on the right to downers on the left. The music matched this arrangement. Ambient effects played at one end, bass and drums at the other.',
-'A spectrum/slope.',
+['I’m not old enough to have actually experienced the rave generation of the late 80’s early 90’s.',
+'The athletes stayed sober. They were already at their peak physiological states.'],
+
+['I’ve just heard myths.',
+'Then the music started. The dance floor was filled in a gradient from uppers on the right to downers on the left.',
 'top', 'https://www.cs.usfca.edu/~galles/visualization/ComparisonSort.html'],
 
-['The energy of the room trickled down from right to left.',
-'It was loaned and passed down from those on your right to those on your left. Everyone sat within their space.'],
+['Secret phone lines and locations, peer to peer flyers.',
+'The music matched this arrangement. Ambient effects played at one end, bass and drums at the other. A spectrum of intensity.'],
 
-['The adventurous ones who dared to move further right quickly returned to their original positions.',
-'Equilibrium.',
-'top', 'https://www.youtube.com/watch?v=Ga1WyWsxD6Y'],
+['A kind of clandestine, colloquial organisation of people looking for a good time.',
+'The adventurous ones who dared to move further right quickly returned to their original positions.'],
 
-['The middle was the sweet spot for the perfectionist.',
-'If you span around as quickly as you could, the song sounded even/=. But this was besides the point.',
-'bottom', 'https://www.cs.cmu.edu/~music/icm-online/readings/panlaws/index.html'],
+['The utopian dreams of that generation still flicker in those that experienced it.',
+'It wasn’t sure what era the music came from. Could’ve been 90s, 00’s, 10’s or 20’s.'],
 
-['It wasn’t sure what era the music came from. Could’ve been 90s, 00’s, 10’s or 20’s.',
+['When I talk to my dad about it I get excited by the optimism that I can tell he once held.',
 'Locating it wouldn’t have given it anymore significance. And people were too far gone to care.',
 'top', 'https://www.shazam.com/gb'],
 
-['It was tinged with this sickly feeling that it wanted you to like it. How can I love_you when you can’t love_yourself?',
-'It\’s not u it\'s me/u.'],
+['At the same time however, I can’t help but cringe at some of it.',
+'It was tinged with this sickly feeling that it wanted you to like it. Someone should have asked it: ‘how can I love you when you can’t love yourself?’'],
 
-['At one point the music stopped and 3 band members got up on stage theremin/trombone/pheromone.',
-'The 3rd was definitely the sex appeal.'],
+['Theres a kind of naivety to it all.',
+'The walls moved around to form small alcoves where people could sit and have private conversations about important things like work, movies or the environment.'],
 
-['They each wore t-shirts that displayed vaguely provocative 4 letter words.',
-'The words need not be spelled out to the **** reader.'],
+['A pseudo-spiritual-political vagueness that should have changed the world by itself and didn’t.',
+'These conversations were mainly for show and didn’t last long. Endon hated being asked what he did.'],
 
-['The walls moved around to form small alcoves where people could sit and have private conversations about important things like work, movies or the weather.',
-'These conversations were mainly for show and didn’t last long.'],
+['This isn’t to say that rave died out by itself.',
+'Endon was a writer but distinctly not an author. He had no authority contrary to popular belief.',
+'top', 'https://en.wikipedia.org/wiki/Spiral_Tribe'],
 
-['Endon hated being asked what he did.',
-'He was hesitant to even call it work. Certainly not a career.'],
+['Legislation and heavy policing seemed an effective method of trampling the could-be revolutionary dreams of the 90s youth by a confused and potentially paranoid government.',
+'He preferred the title writer because of the mechanical implications it carried. Information was presented to him and he translated it into his writing like a cog transferring energy.'],
 
-['Endon was a writer but distinctly not an author.',
-'He had no authority, contrary to popular belief.'],
+['The illegal side-of-the-motorway parties dissipated into the clubs of the inner city where drinks could be sold, entrants could be searched and venues could be regulated.',
+'Who is to say the time can be regulated and divided by machines?'],
 
-['He preferred the title writer because of the mechanical implications it carried.',
-'less<accountability.'],
+['Technology seems to provide the means to generate subcultures across a worldwide network that have been suppressed.',
+'Fictionalised measurement, although initially confusing, allowed Endon some detours from the 24 hour day cycle.'],
 
-['Information was presented to him and he translated it into his writing.',
-'From the outside, it was known that information was lost in translation but he couldn’t help it.'],
+['But in a time where we protest for the right to protest, how do we snap out of this kinky relationship we have with the powers that be?',
+'The world made up of fictionalised measurements but they are usually assigned from above.',
+'bottom', 'https://bills.parliament.uk/bills/2839'],
 
-['People often felt paranoid about what Endon wrote about them but due to his foreign language, they couldn’t actually know what was said or not said.',
-'This didn’t stop them trying to figure it out.'],
+['We have been completely enveloped by this strict syntax.',
+'Electric cars emulating a noise they don’t produce.'],
 
-['Who knew what time went where?',
-'Probably not Endon.'],
-
-['The clock revolved around its hands, telling the watcher the whenabouts according to it’s whereabouts.  As the Earth revolves around the sun the moon revolves around the Earth.',
-'We can feel the revolutions, yet why does today’s feel more substantial than that of yesterday’s?'],
-
-['I’m so bored.',
-'Overstimulated.'],
-
-['Apart from the transition from light to dark and back again, the clock seems to be the only defining feature of the when we are now a resident of.',
-'If we travelled around the world fast enough would it be constantly light or dark?'],
-
-['Maybe we should try it and report back.',
-'All talk.'],
-
-['Perhaps a flexible alternative is needed. Something intensive.',
-'Why within the 10 seconds to midnight on new years eve, does more intensity fill the room than that of every other day of the year?'],
-
-['New Years eve. The prescription of a time. A time to kiss and to drink and to sing.',
-'Big Pharma’s metaphysical expansion.'],
-
-['A prescribed intensity to the exact singular moment that 23:59 on the 31st December becomes 00:00',
-'                            ',
-'top', 'https://time.is/'],
-
-['It is the clock that tells the time.',
-'learn_to_tell the time.'],
-
-['Does o\’clock just mean/= on the clock?',
-'In fact it means ‘of the clock’ which somehow seemed less shallow to Endon.'],
-
-['The new clock should have a pretty face.',
-'A stunner ( ͡°❥ ͡°).'],
-
-['Endon\'s trail of thought was quickly interrupted by a small creature like man who lit up Endon\'s nostrils before occupying his other senses.',
-'Although abrasive, it was not unpleasant.'],
-
-['He started to describe a production company he had once heard of',
-'Although incoherent, his story was a welcome distraction from the surrounding scenery'],
-
-['A group of invisible people decided to make an invisible film.',
-'Actors and crew members were required to wear tap shoes at all times so that they would not bump into each other.'],
-
-['Everyone was distinguished by their smell.',
-'Bloodhound talent spotters.'],
-
-['The lead actress was picked because of her notably beautiful scent.',
-'It’s notes lay somewhere between citrus oranges and waking up on a good day.'],
-
-['Special cinemas were designed to screen these films.',
-'Heritage buildings were knocked down or reoccupied.'],
-
-['Speakers were mounted 360 degrees around a central audience.',
-'Smells were secreted from special ducts in the ceiling.'],
-
-['The audience were required to shower before visiting. After they left, they would carry the many scents of the film around with them.',
-'This allowed the film to leak outside of the cinema.'],
-
-['In turn smell gained a new role within society.',
-'Beauty standards found new territory.'],
-
-['People who passed the audience members on the street were advertised the film nasally.',
-'                            '],
-
-['Horror',
-'It did not smell sweet.'],
-
-['Action.',
-'It smelt like metal and sweat.'],
-
-['Romance.' ,
-'The smell was sickly.'],
-
-[ 'Sci-fi.',
-'There is no sound or smell in the vacuum of space.'],
-
-['Endon was willing to overlook the difficult problem posed by invisible individuals due to the man\'s unworldly aroma',
-'The adjective \'unworldy\' seems to carry a negative connotation when applied to smell but in this case it was very pleasant',
-'bottom', 'https://www.youtube.com/watch?v=eRvfxWRi6qQ&t=16s'],
-
-['A small escape door opened into a room of paintings',
-'Escape was a generous term. Endon didn’t like painting at all.'],
-
-['The limits of painting seem to have been reached.',
-'In a world of drones and pornography, painted images don’t hold as much power as they were once supposed to.'],
-
-['They exist within gloss, satin and matte.',
-'Or within_the_gaps.'],
-
-['As a mode of image creation, painting is no longer a competitor.',
-'It trips itself up.'],
-
-['Perhaps we could imagine a new mode of painting.',
-'Sounds messy.'],
-
-['Functional painting.',
-'Sounds didactic.',
-'top', 'https://www.youtube.com/watch?v=agi4geKb8v8'],
-
-['It is functional in the sense that it exists in relation to behaviour.',
-'Endon is a prime example of why a human centred approach may be short sighted.'],
-
-['What is to stop sound becoming architecture?',
-'A corridor that constantly emits the sound of rain. A software dimension to architecture.'],
-
-['Technologically and mentally.',
-'Perhaps architects of the built world should have discussions with designers of user interfaces.'],
-
-['A new politics of UI design should first be established.',
-'UI is all about domination and control.'],
-
-['Rather than changing space via behaviour or changing behaviour via space, space and behaviour could exist intra-actively. ',
-'Narrative measurement.'], 
-
-['An office is only an office if people do office work there.',
-'And it’s soul crushing.'],
-
-['Perhaps we could host an exhibition of ephemeral artworks.',
-'Portable artworks that could exist at any time in any place.', 
-'top', 'https://www.e-flux.com/architecture/superhumanity/66881/on-snow-dancing/'],
-
-['Listen, John Cage’s silence is playing.',
-'Don’t say that we could be done for copyright infringement.'],
-
-['Some quiet comotion sprang from outside',
-'An accident of some kind involving an electric car and a forest'],
-
-['Maybe we should create sounds for electric cars?',
-'Too many people hit by silence'],
-
-['They decided it would be a good idea to design a sound for the cars.',
-'Solutions seem to create problems.'],
-
-['Or maybe, the sound of the car could be whatever the driver is listening to at the time?',
-'Endon had no real taste in music.'],
-
-['I heard certain kinds of music can make you a more aggressive driver.',
-'Fast music.'],
-
-['The soundtrack of the car creates it’s behaviour.',
-'People’s tastes inform their behaviour. Not the other way around.',
-'bottom', 'https://www.instagram.com/gourmcell/'],
-
-['Perhaps we should create calming sounds then? Like whale noises or something. Or cats purring.',
-'The non-engine purrs.'],
-
-['But then people might just mistake it for a cat.',
-'A big cat.',
-'top', 'https://www.youtube.com/watch?v=_7vML9C3PZk&t=175s'],
-
-['Or mistake a cat for a car. To be hit by a cat.',
-'Probably not that painful.']
+['A down-on-one-knee proposal:',
+'What if they sounded like cats purring?',
+'bottom', 'https://www.youtube.com/watch?v=_7vML9C3PZk&t=183s']
 
 ];
